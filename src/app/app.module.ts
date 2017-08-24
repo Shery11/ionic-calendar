3741dc7effb2543097f422cfd1c94f88303d5d15
shery11 <shery_kirmani@hotmail.com>
2017-08-24 15:23:27 +0500

    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,23 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
+
+
+// for calendar
+import { NgCalendarModule } from 'ionic2-calendar';
+
+
+const firebaseAuth = {
+    apiKey: "AIzaSyDbkgqhc3--XKi3mSmKc_J_bztONps4pF4",
+    authDomain: "ionic-crud-b95b2.firebaseapp.com",
+    databaseURL: "https://ionic-crud-b95b2.firebaseio.com",
+    projectId: "ionic-crud-b95b2",
+    storageBucket: "ionic-crud-b95b2.appspot.com",
+    messagingSenderId: "984894407203"
+  }
 
 @NgModule({
   declarations: [
@@ -14,6 +31,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    NgCalendarModule,
+    AngularFireDatabaseModule,
+    AngularFireModule.initializeApp(firebaseAuth),
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,ModalController,AlertController  } from 'ionic-angular';
+import * as moment from 'moment';
+
+import { AngularFireDatabase} from 'angularfire2/database';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,118 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
 
+   eventSource;
+    viewTitle;
+    events = [];
+    isToday: boolean;
+    selectedDay = new Date();
+    calendar = {
+        mode: 'month',
+        currentDate: new Date()
+    }; // these are the variable used by the calendar.
+   
+
+     path = "/events"
+
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController,public db : AngularFireDatabase) {
+     
+      
+
+      // this.events.push({
+      // 	startTime : new Date(),
+      // 	endTime : new Date()
+      // })
+
+      // // this.events.push({
+      // // 	startTiem : new Date("2017-08-23T12:00:00+05:00"),
+      // // 	endTime : new Date("2017-08-23T12:00:00+05:00")
+      // // })
+
+
+      // this.eventSource = this.events;
+
+      this.db.list(this.path).subscribe(data=>{
+      console.log(data.length);
+      for (var i = 0; i < data.length; ++i) {
+
+        console.log(data[i]);
+        this.loadEvents(data[i]);
+      }
+    },err=>{
+       console.log(err);
+    });
+
+
+      
+      
+  }
+
+   
+
+       loadEvents(data) {
+	        this.events.push({
+	            title: data.title,
+	            startTime: new Date(data.startTime),
+	            endTime: new Date(data.endTime),
+	            allDay: data.allDay
+	        });
+
+	        this.eventSource = this.events;
+        }
+
+    changeMode(mode) {
+        this.calendar.mode = mode;
+    }
+
+  addEvent() {
+    let modal = this.modalCtrl.create('EventModalPage', {selectedDay: this.selectedDay});
+    modal.present();
+    
+
+    modal.onDidDismiss(data => {
+    	console.log(data)
+      
+      if (data) {
+        var dbData = {};
+        Object.assign(dbData,data)
+        let eventData = data;
+ 
+        eventData.startTime = new Date(data.startTime);
+        eventData.endTime = new Date(data.endTime);
+        this.events.push(eventData);
+        this.eventSource = [];
+        
+       
+        setTimeout(() => {
+          this.eventSource = this.events;
+          
+          this.db.list(this.path).push(dbData);
+        });
+      }
+    });
+  }
+ 
+  onViewTitleChanged(title) {
+    this.viewTitle = title;
+  }
+ 
+  onEventSelected(event) {
+
+    let start = moment(event.startTime).format('LLLL');
+    let end = moment(event.endTime).format('LLLL');
+    
+    let alert = this.alertCtrl.create({
+      title: '' + event.title,
+      subTitle: 'From: ' + start + '<br>To: ' + end,
+      buttons: ['OK']
+    })
+    alert.present();
+  }
+ 
+  onTimeSelected(ev) {
+    this.selectedDay = ev.selectedTime;
+    
   }
 
 }
